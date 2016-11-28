42a1ec945da820e097a7b8c1dd4d9de79076ac4f
ilyasprogrammer <rakhimkulov@it-projects.info>
2016-11-28 17:21:12 +0500

    init

---
+++ ingredients/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Ingredients',
+    'version': '1.0',
+    'category': 'Sale',
+    'license': 'AGPL-3',
+    'summary': 'ingredients for product.attribute.value',
+    'author': 'Ilyas',
+    'depends': [
+        'product',
+        'sale',
+        'account',
+    ],
+    'data': [
+    ],
+    'installable': True,
+}
