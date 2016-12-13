7e19f06ac0078ce2855f7c855206e4bc876dd988
root <root@static.172-31-1-100.clients.your-server.de>
2016-12-13 21:52:45 +0300

    shop js

---
+++ baron_shop_js/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Baron Shop Js Enhancements',
+    'version': '1.0',
+    'category': 'sale',
+    'summary': 'Baron Shop Js Enhancements',
+    'description': '''
+Baron Shop Js Enhancements
+=======
+Js Enhancements for Baron Shop 
+    ''',
+    'auto_install': False,
+    'application':True,
+        
+    'author': 'IT Libertas',
+    'website': 'http://itlibertas.net',
+    'depends': [
+        'website_sale'
+    ],
+    'data': [ 
+        'security/ir.model.access.csv',
+        'views/templates.xml',
+            ],
+    'qweb': [ 
+    
+            ],
+    'js': [ 
+
+            ],
+    'demo': [ 
+
+            ],
+    'test': [ 
+
+            ],
+    'license': 'AGPL-3',
+    'images': ['static/description/main.png'],
+    'update_xml': [],
+    'application':True,
+    'installable': True,
+
+}
