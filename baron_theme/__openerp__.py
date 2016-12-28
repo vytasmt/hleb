078b7a22ea3dbcae5ede70d60e7f3e22ef5e393a
ilyasprogrammer <rakhimkulov@it-projects.info>
2016-12-29 03:13:38 +0500

    external_dependencies

--- baron_theme/__openerp__.py
+++ baron_theme/__openerp__.py
@@ -4,14 +4,15 @@
     'description': 'Theme for Baron Llc',
     'category': 'Theme',
     'version': '1.0',
-    'website': 'https://itlibertas.com',
-    'author': 'IT Libertas',
+    'website': '',
+    'author': 'IT Libertas, Ilyas',
     'depends': [
         'website_sale',
         'baron_shop_js',
         'baron_shelf',
         'ingredients',
     ],
+    'external_dependencies': {'python': ['bs4']},
     'data': [
         'views/theme.xml',
         'views/product_view.xml',
